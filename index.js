05c862e16bbb10d01562dc459fefdb13190b2396
Kauã <kauafaranha04@gmail.com>
2023-08-17 20:02:15 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+const path = require('path');
+
+
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+        extended : true
+}));
+
+// Usa o html para renderizar 
+app.engine('html', require('ejs').renderFile);
+//Setar para html
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+var tarefas = ['Arrumar o Quarto','Compras no mercado'];
+
+app.post('/',(req,res)=>{
+    tarefas.push(req.body.tarefa);
+    res.render('index',{tarefasList:tarefas});
+});
+
+
+//path.join e express.staic é para pegar o diretorio 
+
+app.get('/',(req,res)=>{
+
+    res.render('index',{tarefasList:tarefas});
+
+});
+
+app.get('/deletar/:id', (req,res)=>{
+    tarefas = tarefas.filter(function(val,index){
+        if(index != req.params.id){
+            return val;
+        }
+    })
+    res.render('index',{tarefasList:tarefas});
+})
+
+
+
+
+app.listen(5000,()=>{
+    console.log('Server Rodando!');
+})
